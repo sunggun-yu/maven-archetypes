7b28bb4f374c100ffc84fd468f3e202758dac6cb
Sunggun Yu <sunggun.yu@gmail.com>
2012-07-30 08:12:18 +0900

    Simple Java Archetype

---
+++ java-simple/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $package;
+
+/**
+ * Hello World!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
